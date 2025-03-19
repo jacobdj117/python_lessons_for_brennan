1ece4f42e9931ba206afb6faa5ba0a4802ec3c30
Jacob <jacobdj117@gmail.com>
2025-03-19 17:02:54 -0500

    Adds solution to lesson 4 challenge

---
+++ challenges/4_fibonachi_sum.py
@@ -0,0 +1,25 @@
+def get_fib_array(element_count):
+    if element_count == 1: return [1]
+    if element_count == 2: return [1, 1]
+
+    fib_array = [1, 1]
+    last = 1
+    second_last = 1
+
+    for i in range(element_count - 2):
+        new_value = last + second_last
+        fib_array.append(new_value)
+        second_last = last
+        last = new_value
+
+    return fib_array
+
+def sum(fib_array):
+    total = 0
+    for i in fib_array:
+        total = total + i
+    print("The sum is ", total)
+
+element_count = int(input("How far into the fibonachi series are we going? "))
+fib_array = get_fib_array(element_count)
+sum(fib_array)
