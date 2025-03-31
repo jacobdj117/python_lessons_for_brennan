f5ffbe81c7b1712d8170e205371856f27397f02b
Jacob <jacobdj117@gmail.com>
2025-03-31 08:22:39 -0500

    Adds space after times in 6 data

--- data/6_gen.py
+++ data/6_gen.py
@@ -4,7 +4,7 @@ PATH = "C:/Users/Jacob/code/python_lessons_for_brennan/data/6_resource_utilizati
 file = open(PATH, "w")
 
 for i in range(10000):
-    file.write("Readings at time " + str(i) + "seconds:\n")
+    file.write("Readings at time " + str(i) + " seconds:\n")
     file.write("    Temperature: " + str(random.randint(75, 95)) + "C\n")
     file.write("    CPU1:        " + str(random.randint(68, 95)) + "%\n")
     file.write("    CPU2:        " + str(random.randint(5, 30)) + "%\n")
